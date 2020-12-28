3e6e62ea1b7c572be3fe1565b5f9aafbb049035f
Joshen Lim <joshenlimek@gmail.com>
2020-12-28 15:41:46 +0800

    Update og meta tags and spice up landing page

---
+++ icons/Star.js
@@ -0,0 +1,16 @@
+const Star = () => (
+  <svg
+    viewBox="0 0 24 24"
+    width="20"
+    height="20"
+    stroke="#FFFFFF"
+    strokeWidth="2"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+  </svg>
+)
+
+export default Star
--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,9 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="stylesheet" href="/uPlot/uPlot.min.css" />
+          <meta property="og:title" content="repository.surf" key="title" />
+          <meta property="og:description" content="Get insights across your organization's repositories" key="description" />
+          <meta property="og:image" content="og-image.png" />
           <script type="text/javascript" src="/uPlot/uPlot.iife.min.js" />
         </Head>
         <body className="bg-gray-700">
