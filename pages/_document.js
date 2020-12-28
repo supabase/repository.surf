dc4705ed65a138986d5eb1bb956a3bb34e0efd1f
Joshen Lim <joshenlimek@gmail.com>
2020-12-28 15:46:55 +0800

    Update og meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,26 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="stylesheet" href="/uPlot/uPlot.min.css" />
-          <meta property="og:title" content="repository.surf" key="title" />
-          <meta property="og:description" content="Get insights across your organization's repositories" key="description" />
+
+          {/* Primary Meta Tags */}
+          <title>repository.surf | Powered by Supabase</title>
+          <meta name="title" content="repository.surf | Powered by Supabase" />
+          <meta name="description" content="Get insights across your organization's repositories" />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://repository.surf/" />
+          <meta property="og:title" content="repository.surf | Powered by Supabase" />
+          <meta property="og:description" content="Get insights across your organization's repositories" />
           <meta property="og:image" content="og-image.png" />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://repository.surf/" />
+          <meta property="twitter:title" content="repository.surf | Powered by Supabase" />
+          <meta property="twitter:description" content="Get insights across your organization's repositories" />
+          <meta property="twitter:image" content="og-image.png" />
+
           <script type="text/javascript" src="/uPlot/uPlot.iife.min.js" />
         </Head>
         <body className="bg-gray-700">
