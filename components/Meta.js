68cab238e844e375d39582b149b7a38cfce71c94
Joshen Lim <joshenlimek@gmail.com>
2020-12-28 16:06:36 +0800

    Update og meta tags

--- components/Meta.js
+++ components/Meta.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 
 const Meta = () => {
-  const { basePath } = useRouter()
   return (
     <Head>
       {/* Primary Meta Tags */}
@@ -15,14 +13,14 @@ const Meta = () => {
       <meta property="og:url" content="https://repository.surf/" />
       <meta property="og:title" content="repository.surf | Powered by Supabase" />
       <meta property="og:description" content="Get insights across your organization's repositories" />
-      <meta property="og:image" content={`${basePath}/og-image.png`} />
+      <meta property="og:image" content={`${window.location.origin}/og-image.png`} />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://repository.surf/" />
       <meta property="twitter:title" content="repository.surf | Powered by Supabase" />
       <meta property="twitter:description" content="Get insights across your organization's repositories" />
-      <meta property="twitter:image" content={`${basePath}/og-image.png`} />
+      <meta property="twitter:image" content={`${window.location.origin}/og-image.png`} />
     </Head>
   )
 }
