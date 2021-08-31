24f61de8611dee212937608e03e0c00055ad3e8a
Joshen Lim <joshenlimek@gmail.com>
2021-08-31 18:12:16 +0800

    Update Star History to show both cumulative counts and daily new counts

--- components/StarHistory.js
+++ components/StarHistory.js
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import { Icon } from '@supabase/ui'
-import TimelineChart from '~/components/TimelineChart'
-import Pill from 'components/Pill'
-import { retrieveStarGrowthToday, retrieveStarGrowthMonth } from 'lib/helpers' 
+import { useState } from "react";
+import { Icon } from "@supabase/ui";
+import TimelineChart from "~/components/TimelineChart";
+import Pill from "components/Pill";
+import { retrieveStarGrowthToday, retrieveStarGrowthMonth } from "lib/helpers";
 
 const StarHistory = ({
   starHistoryRef,
-  header = 'Stars',
+  header = "Stars",
   embed = false,
   enableSharing = true,
   repoName,
@@ -19,55 +19,112 @@ const StarHistory = ({
   noReposSelected,
   onOpenShare = () => {},
 }) => {
+  const [selectedTimeFilter, setSelectedTimeFilter] = useState("allTime");
 
   const options = [
     {
-      key: 'cumulative_counts',
-      label: 'Cumulative counts'
+      key: "cumulative_counts",
+      label: "Cumulative counts",
     },
     {
-      key: 'daily_new_counts',
-      label: 'Daily new counts'
-    }
-  ] 
-  
-  const [chartType, setChartType] = useState(options[0].key)
+      key: "daily_new_counts",
+      label: "Daily new counts",
+    },
+  ];
+
+  const timeOptions = [
+    {
+      key: "allTime",
+      label: "All time",
+    },
+    {
+      key: "pastNinetyDays",
+      label: "Past 90 days",
+      duration: 90 * 24 * 60 * 60 * 1000,
+    },
+    {
+      key: "pastThirtyDays",
+      label: "Past 30 days",
+      duration: 30 * 24 * 60 * 60 * 1000,
+    },
+    {
+      key: "pastWeek",
+      label: "Past week",
+      duration: 7 * 24 * 60 * 60 * 1000,
+    },
+  ];
+
+  const deriveOptions = (data) => {
+    const currentTime = new Date();
+    const formattedData = data.map((row) => {
+      return { ...row, date: new Date(row["date"]).toISOString() };
+    });
+    return timeOptions.filter((option) => {
+      if (option.key === "allTime") return option;
+      else {
+        const cutoffDate = new Date(currentTime - option.duration);
+        const filteredData = formattedData.filter((row) => {
+          if (row["date"] >= cutoffDate.toISOString()) return row;
+        });
+        if (filteredData.length !== data.length) return option;
+      }
+    });
+  };
 
   const renderTimelineChart = () => {
     if (starHistory.length == 0) {
       return (
         <div className="py-24 lg:py-36 flex items-center justify-center text-gray-400">
-          {noStarHistory ? 'Selected repositories have no stars' : 'Select a repository first to view its star history'}
+          {noStarHistory
+            ? "Selected repositories have no stars"
+            : "Select a repository first to view its star history"}
         </div>
-      )
+      );
     }
-    
+
     return (
       <>
-        <TimelineChart
-          id="starHistoryChart"
-          uPlot={uPlot}
-          data={starHistory}
-          chartType={chartType}
-          dateKey="date"
-          valueKey="starNumber"
-          xLabel="Number of stars"
-          showOnlyDate={true}
-          renderAdditionalActions={() => {
-            return (
-              <div className="space-x-2 flex items-center">
-                {options.map((option) => (
-                  <Pill
-                    key={option.key}
-                    label={option.label}
-                    selected={option.key === chartType}
-                    onSelectPill={() => setChartType(option.key)}
-                  />
-                ))}
-              </div>
-            )
-          }}
-        />
+        <div className="flex flex-col">
+          <div className="px-7 pb-5 flex items-center lg:mb-0">
+            {deriveOptions(starHistory).map((option) => (
+              <Pill
+                key={option.key}
+                label={option.label}
+                selected={option.key === selectedTimeFilter}
+                withoutBorder={true}
+                onSelectPill={() => setSelectedTimeFilter(option.key)}
+              />
+            ))}
+          </div>
+          <p className="w-full text-white text-right pr-6">Cumulative counts</p>
+          <TimelineChart
+            id="starHistoryChart1"
+            uPlot={uPlot}
+            data={starHistory}
+            chartType={options[0].key}
+            dateKey="date"
+            valueKey="starNumber"
+            xLabel="Number of stars"
+            showOnlyDate={true}
+            showTimeFilter={false}
+            selectedTimeFilter={selectedTimeFilter}
+          />
+          <p className="mt-10 w-full text-white text-right pr-6">
+            Daily new counts
+          </p>
+          <TimelineChart
+            id="starHistoryChart2"
+            uPlot={uPlot}
+            data={starHistory}
+            chartType={options[1].key}
+            dateKey="date"
+            valueKey="starNumber"
+            xLabel="Number of stars"
+            showOnlyDate={true}
+            showTimeFilter={false}
+            selectedTimeFilter={selectedTimeFilter}
+          />
+        </div>
         {!embed && (
           <div className="sm:px-10 w-full mt-6 flex flex-col">
             <p className="text-white">Growth statistics</p>
@@ -75,22 +132,30 @@ const StarHistory = ({
               <div className="col-span-6 sm:col-span-5 xl:col-span-4">
                 <p className="text-gray-400">Past day</p>
                 <div id="numbers" className="flex items-center mt-2">
-                  <p className="text-white text-3xl mr-2">{retrieveStarGrowthToday(starHistory)}</p>
+                  <p className="text-white text-3xl mr-2">
+                    {retrieveStarGrowthToday(starHistory)}
+                  </p>
                 </div>
               </div>
               <div className="col-span-6 sm:col-span-5 xl:col-span-4">
                 <p className="text-gray-400">For the month</p>
-                <p className="mt-2 text-white text-3xl">{retrieveStarGrowthMonth(starHistory)}</p>
+                <p className="mt-2 text-white text-3xl">
+                  {retrieveStarGrowthMonth(starHistory)}
+                </p>
               </div>
             </div>
           </div>
         )}
       </>
-    )
-  }
+    );
+  };
 
   return (
-    <div ref={starHistoryRef} id="starHistory" className={`w-full ${embed ? '' : 'mb-12 lg:mb-20'}`}>
+    <div
+      ref={starHistoryRef}
+      id="starHistory"
+      className={`w-full ${embed ? "" : "mb-12 lg:mb-20"}`}
+    >
       {!embed && (
         <div className="pb-5 sm:px-10 sm:pb-7">
           <div className="flex items-center justify-between">
@@ -98,53 +163,90 @@ const StarHistory = ({
               <h1>{header}</h1>
               {enableSharing && (
                 <div className="hidden lg:flex items-center ml-3 transition opacity-0 group-hover:opacity-100">
-                  <div className="cursor-pointer" onClick={() => onOpenShare('stars')}>
-                    <Icon type="Share2" size={20} strokeWidth={2} className="text-gray-400" />
+                  <div
+                    className="cursor-pointer"
+                    onClick={() => onOpenShare("stars")}
+                  >
+                    <Icon
+                      type="Share2"
+                      size={20}
+                      strokeWidth={2}
+                      className="text-gray-400"
+                    />
                   </div>
                 </div>
               )}
             </div>
-            {!loadingStarHistory && lastUpdated !== null && starHistory.length > 0 && (
-              <div className="flex items-center">
-                <Icon type="Star" size={20} strokeWidth={2} className="text-white" />
-                <span className="ml-2 text-white">{totalStarCount}</span>
+            {!loadingStarHistory &&
+              lastUpdated !== null &&
+              starHistory.length > 0 && (
+                <div className="flex items-center">
+                  <Icon
+                    type="Star"
+                    size={20}
+                    strokeWidth={2}
+                    className="text-white"
+                  />
+                  <span className="ml-2 text-white">{totalStarCount}</span>
+                </div>
+              )}
+          </div>
+          <p className="mt-2 text-base text-gray-400">
+            This is a timeline of how the star count of {repoName} has grown
+            till today.
+          </p>
+          {!loadingStarHistory &&
+            lastUpdated === null &&
+            starHistory.length > 0 && (
+              <div className="mt-5 flex item-center text-xs text-gray-400">
+                <Icon
+                  type="Loader"
+                  className="animate-spin text-white inline"
+                  size={18}
+                  strokeWidth={2}
+                />
+                <span className="ml-2 transform translate-x-0.5 translate-y-0.5 inline-block">
+                  Loading data (
+                  {(
+                    (starHistory[starHistory.length - 1].starNumber /
+                      totalStarCount) *
+                    100
+                  )
+                    .toString()
+                    .slice(0, 5)}
+                  % complete)
+                </span>
               </div>
             )}
-          </div>
-          <p className="mt-2 text-base text-gray-400">This is a timeline of how the star count of {repoName} has grown till today.</p>
-          {!loadingStarHistory && lastUpdated === null && starHistory.length > 0 && (
-            <div className="mt-5 flex item-center text-xs text-gray-400">
-              <Icon type="Loader" className="animate-spin text-white inline" size={18} strokeWidth={2} />
-              <span className="ml-2 transform translate-x-0.5 translate-y-0.5 inline-block">
-                Loading data (
-                  {(starHistory[starHistory.length - 1].starNumber / totalStarCount * 100).toString().slice(0, 5)
-                }% complete)
-              </span>
-            </div>
-          )}
         </div>
       )}
       <div className="flex-1 flex flex-col items-start">
-        <div className={`w-full ${embed ? '' : 'pb-3 sm:pb-0 sm:pr-3'}`}>
-          {loadingStarHistory
-            ? (
-              noReposSelected ?
+        <div className={`w-full ${embed ? "" : "pb-3 sm:pb-0 sm:pr-3"}`}>
+          {loadingStarHistory ? (
+            noReposSelected ? (
               <div className="py-24 lg:py-36 flex items-center justify-center text-gray-400">
                 Select a repository first to view its star history
-              </div> :
+              </div>
+            ) : (
               <div className="py-24 lg:py-32 text-white w-full flex flex-col items-center justify-center">
-                <Icon type="Loader" className="animate-spin text-white" size={24} strokeWidth={2} />
+                <Icon
+                  type="Loader"
+                  className="animate-spin text-white"
+                  size={24}
+                  strokeWidth={2}
+                />
                 <p className="text-xs mt-3 leading-5 text-center">
                   {loadingMessage || "Retrieving repository star history"}
                 </p>
               </div>
             )
-            : (<>{renderTimelineChart()}</>)
-          }
+          ) : (
+            <>{renderTimelineChart()}</>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StarHistory
+export default StarHistory;
--- components/TimelineChart.js
+++ components/TimelineChart.js
@@ -1,29 +1,29 @@
-import { useEffect, useRef, useState } from 'react'
-import Toggle from 'components/Toggle'
-import Pill from 'components/Pill'
-import { convertCumulativeToDailyNewStars } from '~/lib/helpers'
+import { useEffect, useRef, useState } from "react";
+import Toggle from "components/Toggle";
+import Pill from "components/Pill";
+import { convertCumulativeToDailyNewStars } from "~/lib/helpers";
 
 const options = [
   {
-    key: 'allTime',
-    label: 'All time',
+    key: "allTime",
+    label: "All time",
   },
   {
-    key: 'pastNinetyDays',
-    label: 'Past 90 days',
-    duration: 90*24*60*60*1000,
+    key: "pastNinetyDays",
+    label: "Past 90 days",
+    duration: 90 * 24 * 60 * 60 * 1000,
   },
   {
-    key: 'pastThirtyDays',
-    label: 'Past 30 days',
-    duration: 30*24*60*60*1000,
+    key: "pastThirtyDays",
+    label: "Past 30 days",
+    duration: 30 * 24 * 60 * 60 * 1000,
   },
   {
-    key: 'pastWeek',
-    label: 'Past week',
-    duration: 7*24*60*60*1000,
+    key: "pastWeek",
+    label: "Past week",
+    duration: 7 * 24 * 60 * 60 * 1000,
   },
-]
+];
 
 const TimelineChart = ({
   id,
@@ -31,108 +31,112 @@ const TimelineChart = ({
   data,
   dateKey,
   valueKey,
-  chartType = null,
-  xLabel = '',
+  selectedTimeFilter,
+  chartType,
+  xLabel = "",
   showOnlyDate = false,
   showBaselineToggle = false,
   showTimeFilter = true,
   renderAdditionalActions = null,
 }) => {
-  const dataPlotRef = useRef(null)
-  const [isBaselineZero, setIsBaselineZero] = useState(false)
-  const [selectedTimeFilter, setSelectedTimeFilter] = useState('allTime')
+  const dataPlotRef = useRef(null);
+  const [isBaselineZero, setIsBaselineZero] = useState(false);
+  const [, setSelectedTimeFilter] = useState("allTime");
 
   const deriveOptions = () => {
-    const currentTime = new Date()
-    const formattedData = data.map(row => {
-      return { ...row, [dateKey]: new Date(row[dateKey]).toISOString() }
-    })
-    return options.filter(option => {
-      if (option.key === 'allTime') return option
+    const currentTime = new Date();
+    const formattedData = data.map((row) => {
+      return { ...row, [dateKey]: new Date(row[dateKey]).toISOString() };
+    });
+    return options.filter((option) => {
+      if (option.key === "allTime") return option;
       else {
-        const cutoffDate = new Date(currentTime - option.duration)
-        const filteredData = formattedData.filter(row => {
-          if (row[dateKey] >= cutoffDate.toISOString() ) return row
-        })
-        if (filteredData.length !== data.length) return option
+        const cutoffDate = new Date(currentTime - option.duration);
+        const filteredData = formattedData.filter((row) => {
+          if (row[dateKey] >= cutoffDate.toISOString()) return row;
+        });
+        if (filteredData.length !== data.length) return option;
       }
-    })
-  }
+    });
+  };
 
   const handleToggle = () => {
-    setIsBaselineZero(!isBaselineZero)
-  }
+    setIsBaselineZero(!isBaselineZero);
+  };
 
   useEffect(() => {
     if (data.length > 0) {
-
-      const currentTime = new Date()
-      const formattedData = data.map(row => {
-        return { ...row, [dateKey]: new Date(row[dateKey]).toISOString() }
-      })
-      let filteredData = formattedData.slice()
+      const currentTime = new Date();
+      const formattedData = data.map((row) => {
+        return { ...row, [dateKey]: new Date(row[dateKey]).toISOString() };
+      });
+      let filteredData = formattedData.slice();
 
       // If the chartType exists and is 'daily_new_counts', then
       // convert our data from cumulative to daily increase counts.
       // NOTE: convertCumulativeToDailyNewStars currently only works
       // for star counts.
       // ASSUMPTION: When looking at star counts, the data is already sorted.
-      if (chartType && chartType === 'daily_new_counts') {
-        filteredData = convertCumulativeToDailyNewStars(filteredData)
+      if (chartType && chartType === "daily_new_counts") {
+        filteredData = convertCumulativeToDailyNewStars(filteredData);
       }
 
       // We'll show data after the cutoffDate if other than allTime is selected
-      if (selectedTimeFilter != 'allTime') {
-        const [selectedTimeFilterOption] = options.filter(option => option.key === selectedTimeFilter)
-        const cutoffDate = new Date(currentTime - selectedTimeFilterOption.duration)
-        filteredData = filteredData.filter(row => {
-          if (row[dateKey] >= cutoffDate.toISOString() ) return row
-        })
+      if (selectedTimeFilter !== "allTime") {
+        const [selectedTimeFilterOption] = options.filter(
+          (option) => option.key === selectedTimeFilter
+        );
+        const cutoffDate = new Date(
+          currentTime - selectedTimeFilterOption.duration
+        );
+        filteredData = filteredData.filter((row) => {
+          if (row[dateKey] >= cutoffDate.toISOString()) return row;
+        });
       }
 
       const sortedData = filteredData.sort((a, b) => {
-        return (a[dateKey] < b[dateKey]) ? -1 : ((a[dateKey] > b[dateKey]) ? 1 : 0);
-      })
-      const chartData = [[], []]
-      sortedData.forEach(row => {
-        chartData[0].push(Math.floor(new Date(row[dateKey]) / 1000))
-        chartData[1].push(row[valueKey])
-      })
+        return a[dateKey] < b[dateKey] ? -1 : a[dateKey] > b[dateKey] ? 1 : 0;
+      });
+      const chartData = [[], []];
+      sortedData.forEach((row) => {
+        chartData[0].push(Math.floor(new Date(row[dateKey]) / 1000));
+        chartData[1].push(row[valueKey]);
+      });
       // sortedData format example:
       // [{<dateKey>: "2019-10-15T00:00:00.000Z", <valueKey>: 1},
       //  {<dateKey>: "2020-01-15T00:00:00.000Z", <valueKey>: 2},
       //  {<dateKey>: "2020-01-16T00:00:00.000Z", <valueKey>: 15}]
-      
+
       // We need to pass true as the second argument of setData
       // in order to reset the scale.
-      dataPlotRef.current?.setData(chartData, true)
+      dataPlotRef.current?.setData(chartData, true);
 
       if (isBaselineZero) {
-        dataPlotRef.current?.setScale('y', {
+        dataPlotRef.current?.setScale("y", {
           min: 0,
-          max: Math.max(...data.map(issue => issue[valueKey]))
-        })
+          max: Math.max(...data.map((issue) => issue[valueKey])),
+        });
       }
-      
     }
-  }, [data, valueKey, chartType, isBaselineZero, selectedTimeFilter])
+  }, [data, valueKey, chartType, isBaselineZero, selectedTimeFilter]);
 
   useEffect(() => {
     if (dataPlotRef.current === null) {
-      const chartDiv = document.getElementById(id)
+      const chartDiv = document.getElementById(id);
 
       let opts = {
-        id: "chart1",
-        class: "my-chart",
+        id,
+        class: `${id}_chart`,
         width: chartDiv.offsetWidth,
         height: chartDiv.offsetHeight - 30,
         cursor: {
           points: {
-            size:   (u, seriesIdx)       => u.series[seriesIdx].points.size * 2.5,
-            width:  (u, seriesIdx, size) => size / 4,
-            stroke: (u, seriesIdx)       => u.series[seriesIdx].points.stroke(u, seriesIdx) + '90',
-            fill:   (u, seriesIdx)       => "#fff",
-          }
+            size: (u, seriesIdx) => u.series[seriesIdx].points.size * 2.5,
+            width: (u, seriesIdx, size) => size / 4,
+            stroke: (u, seriesIdx) =>
+              u.series[seriesIdx].points.stroke(u, seriesIdx) + "90",
+            fill: (u, seriesIdx) => "#fff",
+          },
         },
         scales: {
           x: {},
@@ -141,11 +145,15 @@ const TimelineChart = ({
         series: [
           {
             value: (self, rawValue) => {
-              const date = new Date(rawValue * 1000)
+              const date = new Date(rawValue * 1000);
               return showOnlyDate
                 ? date.toLocaleDateString()
-                : `${date.toLocaleDateString()} ${date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`
-            }
+                : `${date.toLocaleDateString()} ${date.toLocaleString("en-US", {
+                    hour: "numeric",
+                    minute: "numeric",
+                    hour12: true,
+                  })}`;
+            },
           },
           {
             // initial toggled state (optional)
@@ -158,7 +166,7 @@ const TimelineChart = ({
             width: 1,
             fill: "rgba(0, 186, 85, 0.3)",
             dash: [10, 5],
-          }
+          },
         ],
         axes: [
           // x-axis
@@ -175,69 +183,74 @@ const TimelineChart = ({
             size: 90,
             grid: {
               width: 0,
-              stroke: "transparent"
-            }
-          }
+              stroke: "transparent",
+            },
+          },
         ],
       };
 
-      const chartData = [[], []]
+      const chartData = [[], []];
 
-      const formattedData = data.map(row => {
-        return { ...row, [dateKey]: new Date(row[dateKey]).toISOString() }
-      })
-      
-      const sortedData = formattedData.sort((a, b) => {
-        return (a[dateKey] < b[dateKey]) ? -1 : ((a[dateKey] > b[dateKey]) ? 1 : 0);
-      })
+      const formattedData = data.map((row) => {
+        return { ...row, [dateKey]: new Date(row[dateKey]).toISOString() };
+      });
 
-      sortedData.forEach(issue => {
-        chartData[0].push(Math.floor(new Date(issue[dateKey]) / 1000))
-        chartData[1].push(issue[valueKey])
-      })
-    
-      const plot = new uPlot(opts, chartData, document.getElementById(id))
-      dataPlotRef.current = plot
+      let filteredData = formattedData.slice();
+      if (chartType && chartType === "daily_new_counts") {
+        filteredData = convertCumulativeToDailyNewStars(filteredData);
+      }
+
+      const sortedData = filteredData.sort((a, b) => {
+        return a[dateKey] < b[dateKey] ? -1 : a[dateKey] > b[dateKey] ? 1 : 0;
+      });
+
+      sortedData.forEach((issue) => {
+        chartData[0].push(Math.floor(new Date(issue[dateKey]) / 1000));
+        chartData[1].push(issue[valueKey]);
+      });
+
+      const plot = new uPlot(opts, chartData, document.getElementById(id));
+      dataPlotRef.current = plot;
     }
 
-    const legend = dataPlotRef.current?.root.querySelector(".u-legend")
-    const chartRegion = dataPlotRef.current?.root.querySelector(".u-over")
-    if (legend) legend.style.visibility = 'hidden'
+    const legend = dataPlotRef.current?.root.querySelector(".u-legend");
+    const chartRegion = dataPlotRef.current?.root.querySelector(".u-over");
+    if (legend) legend.style.visibility = "hidden";
 
     const handleResize = () => {
-      const chartDiv = document.getElementById(id)
+      const chartDiv = document.getElementById(id);
       dataPlotRef.current?.setSize({
         width: chartDiv.offsetWidth,
-        height: chartDiv.offsetHeight - 30
-      })
-    }
+        height: chartDiv.offsetHeight - 30,
+      });
+    };
 
     const handleMouseEnter = () => {
-      if (legend) legend.style.visibility = 'visible'
-    }
+      if (legend) legend.style.visibility = "visible";
+    };
 
     const handleMouseLeave = () => {
-      if (legend) legend.style.visibility = 'hidden'
-    }
+      if (legend) legend.style.visibility = "hidden";
+    };
 
-    window.addEventListener("resize", handleResize)
-    chartRegion.addEventListener("mouseenter", handleMouseEnter)
-    chartRegion.addEventListener("mouseleave", handleMouseLeave)
+    window.addEventListener("resize", handleResize);
+    chartRegion.addEventListener("mouseenter", handleMouseEnter);
+    chartRegion.addEventListener("mouseleave", handleMouseLeave);
     return () => {
-      window.removeEventListener("resize", handleResize)
-      chartRegion.removeEventListener("mouseenter", handleMouseEnter)
-      chartRegion.removeEventListener("mouseleave", handleMouseLeave)
-    }
-  }, [])
+      window.removeEventListener("resize", handleResize);
+      chartRegion.removeEventListener("mouseenter", handleMouseEnter);
+      chartRegion.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
 
-  const chartMinValue = Math.min(...data.map(issue => issue[valueKey]))
-  
+  const chartMinValue = Math.min(...data.map((issue) => issue[valueKey]));
+
   return (
     <>
       <div className="sm:px-10 mb-3 flex flex-col items-start lg:flex-row lg:items-center lg:justify-between">
         {showTimeFilter && (
           <div className="flex items-center mb-5 lg:mb-0">
-            {deriveOptions().map(option => (
+            {deriveOptions().map((option) => (
               <Pill
                 key={option.key}
                 label={option.label}
@@ -260,14 +273,14 @@ const TimelineChart = ({
             <Toggle
               isOn={isBaselineZero}
               onToggle={handleToggle}
-              label="Set baseline to 0" 
+              label="Set baseline to 0"
               labelPosition="right"
             />
           )}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default TimelineChart
+export default TimelineChart;
